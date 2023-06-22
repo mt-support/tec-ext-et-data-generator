36f906ae931fe0c45b4c1ac6cb65d9d342529ca0
Camwyn <stephen@theeventscalendar.com>
2023-06-22 14:47:32 -0400

    fix ET req version

--- src/Tribe/Plugin_Register.php
+++ src/Tribe/Plugin_Register.php
@@ -26,7 +26,7 @@ class Plugin_Register extends Abstract_Plugin_Register {
 	protected $main_class   = Plugin::class;
 	protected $dependencies = [
 		'parent-dependencies' => [
-			'Tribe__Tickets__Main' => '6.1.2-dev',
+			'Tribe__Tickets__Main' => '5.6.1-dev',
 		],
 	];
 }
